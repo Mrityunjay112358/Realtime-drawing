bdbed42e050d73b803adad0c2f8c3ea8ed1fb7ac
Mrityunjay112358 <73526816+Mrityunjay112358@users.noreply.github.com>
2021-05-15 19:14:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,21 @@ function setup(){
     canvas.position(680,70)
     video = createCapture(VIDEO);
     video.size(600,500);
+    //initializing posenet
+    posenet = ml5.poseNet(video,modelLoaded);
+    //executing posenet
+    posenet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("model is initialized");
+}
+
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+    nosex = results[0].pose.nose.x;
+    nosey = results[0].pose.nose.y;
+    console.log("noseX = "+nosex+" noseY = "+nosey)
+}
 }
