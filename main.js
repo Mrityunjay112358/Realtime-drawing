4f8e4b44d9a30512af9a6acddfcf3a6a09a9ae0a
Mrityunjay112358 <73526816+Mrityunjay112358@users.noreply.github.com>
2021-05-17 12:50:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ nosey=0;
 difference=0;
 rightwristx=0;
 leftwristx=0;
-
+eyex= 0;
+eyey=0;
+balloon;
+function preload(){
+    balloon = loadImage("balloon.png");
+}
 function setup(){
     canvas = createCanvas(600,500);
-    canvas.position(680,70)
+    canvas.position(680,120);
     video = createCapture(VIDEO);
     video.size(600,500);
     //initializing posenet
@@ -24,6 +29,27 @@ if(results.length>0){
     console.log(results);
     nosex = results[0].pose.nose.x;
     nosey = results[0].pose.nose.y;
-    console.log("noseX = "+nosex+" noseY = "+nosey)
+    eyex = results[0].pose.rightEye.x;
+    eyey = results[0].pose.rightEye.y;
+    console.log("noseX = "+nosex+" noseY = "+nosey);
+    leftwristx = results[0].pose.leftWrist.x;
+    rightwristx = results[0].pose.rightWrist.x;
+    difference = floor(leftwristx - rightwristx);
+
+    console.log("leftwristx = "+leftwristx+" rightwristx = "+rightwristx+" difference = "+difference);
 }
+}
+
+function draw(){
+    background("#f2e56b");
+    document.getElementById("sqaure_side").innerHTML = "The Side Of The Square is "+difference+"px";
+    fill("lightblue");
+    //border colour
+    stroke("blue");
+    //border width
+    strokeWeight(4);
+    rectMode(CENTER);
+   square(nosex,nosey,difference);
+circle(nosex,nosey,5);
+image(balloon,eyex,eyey,60,100);
 }
